18bd69f669893c535b9f98a1d0381d69430505d8
Sébastien Lehmstedt <lehmstedt-sebastien@hotmail.fr>
2018-09-01 22:33:56 +0200

    Initialize primeng

--- curriculum-vitae-front/src/app/app.module.ts
+++ curriculum-vitae-front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
